b024be42ecf23e53f6e330b6fea5e2e1952bda49
cyxv <weariercomic78@gmail.com>
2022-08-19 02:34:27 +0000

    indent changes

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
+from disnake.ext import commands
+import disnake
+import time
 global token
 with open("token.txt") as f:
     token = f.read()
 
-import time
 timerStart = time.time()
-
-import disnake
-from disnake.ext import commands
 
 client = commands.Bot(command_prefix="!")
 
 extensions = ["cogs.general", "jishaku"]
 
 for extension in extensions:
-  try:
-    print('Attempting to load extension "{}"'.format(extension))
-    client.load_extension(extension)
-  except Exception as e:
-    print('Failed to load extension "{}" ({})'.format(extension, e))
-    
+    try:
+        print('Attempting to load extension "{}"'.format(extension))
+        client.load_extension(extension)
+    except Exception as e:
+        print('Failed to load extension "{}" ({})'.format(extension, e))
+
+
 @client.event
 async def on_ready():
-  sec = round(time.time() - timerStart, 4)
-  print("Bot loaded in {} seconds.".format(sec))
-  
+    sec = round(time.time() - timerStart, 4)
+    print("Bot loaded in {} seconds.".format(sec))
+
 print("Starting bot...")
 client.run(token)
