13e16f64db71855613ef142bc37805642a7d26bb
cyxv <weariercomic78@gmail.com>
2022-08-19 02:37:51 +0000

    disnake part 2

--- cogs/general.py
+++ cogs/general.py
@@ -1,15 +1,15 @@
-import discord
-from discord.ext import commands
+import disnake
+from disnake.ext import commands
 
 class General(commands.Cog):
-  def __init__(self, client):
-    self.bot = client
-    self._last_member = None
-    
-  @commands.command()
-  async def ping(self, ctx):
-    latency = round(self.bot.latency * 1000)
-    await ctx.send("Pong! Latency is {}ms.".format(latency)) 
-    
-  def setup(client):
-    client.add_cog(General(client))
+    def __init__(self, client):
+        self.bot = client
+        self._last_member = None
+
+    @commands.command()
+    async def ping(self, ctx):
+        latency = round(self.bot.latency * 1000)
+        await ctx.send("Pong! Latency is {}ms.".format(latency))
+
+    def setup(client):
+        client.add_cog(General(client))
